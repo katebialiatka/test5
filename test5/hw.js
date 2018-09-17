64566f1e8ad00d50357dc6e8488ad0afcf71d446
Yury Klyshevich <Yury_Klyshevich@epam.com>
2018-09-17 10:32:18 +0300

    edit hw

--- test5/hw.js
+++ test5/hw.js
@@ -1,41 +1,6 @@
-function Test-PSScript
-{
-    param(
-        [Parameter(Mandatory=$true, Position=0, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
-        [ValidateNotNullOrEmpty()]
-        [Alias('PSPath', 'FullName')]
-        [System.String[]] $FilePath,
-        [Switch]$IncludeSummaryReport
-    )
-    begin
-    {
-        $total=$fails=0
-    }
-    process
-    {
-        $FilePath|ForEach-Object{
-            if(Test-Path -Path $_ -PathType Leaf)
-            {
-                $Path = Convert-Path -Path $_
-                $Errors = $null
-                $Content = Get-Content -Path $Path
-                $Tokens = [System.Management.Automation.PSParser]::Tokenize($Content, [ref] $Errors)
-                if ($Errors) 
-                {
-                    $fails+=1
-                    $Errors|ForEach-Object{
-                        $_.Token|Add-Member -MemberType NoteProperty -Name Path -Value $Path -PassThru|Add-Member -MemberType NoteProperty -Name ErrorMessage -Value $_.Message -PassThru
-                    }
-                }
-                $total+=1
-            }
-        }
-    }
-    end
-    {
-        if ($IncludeSummaryReport)
-        {
-            Write-Host "$total script(s) processed, $fails script(s) contain syntax errors."
-        }
-    }
-}
+var http = require('http');
+
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.end('Hello World');
+}).listen(8080)
