a407f844387c235f7b51bc24e1f36fafbe523277
yuryklyshevich <yura.klyshevich1@gmail.com>
2018-11-20 09:52:24 +0300

    the second

--- node/hw.js
+++ node/hw.js
@@ -1,8 +1,6 @@
 var http = require('http');
 
-http.createServ
-er(func
-tion (req, res) {
+http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.end('Hello World');
-}).listen(8080
+}).listen(8080)
